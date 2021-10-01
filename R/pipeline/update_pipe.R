aa19e010ae4289f852ba3ffe84cd1bf07e223587
Giuliano Sposito <gsposito@ciandt.com>
2021-10-01 09:06:36 -0300

    Atualização do pipe de update para ficar igual ao de new week.

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -4,21 +4,15 @@ library(glue)
 library(ffanalytics)
 library(flexdashboard)
 library(yaml)
-library(tidyverse)
-library(lubridate)
-library(glue)
-library(ffanalytics)
-library(flexdashboard)
-library(yaml)
 
 # Suppress summarise info
 options(dplyr.summarise.inform = FALSE)
 
 # EXECUTION PARAMETERS ####
-week <- 3
+week <- 4
 season <- 2021
 config <- read_yaml("./config/config.yml")
-prefix <- "final"
+prefix <- "posTNF"
 destPath <- "static/reports/2021"
 sim.version <- 5
 
@@ -26,9 +20,11 @@ sim.version <- 5
 source("./R/import/checkFantasyAPI.R")
 if(!checkFantasyAPI(config$authToken, config$leagueId, week)) stop("Unable to access Fantasy API!")
 
-# TABELA DE PROJECAO ####
-source("./R/import/ffa_player_projection.R")
+# SCRAP: RECOVER
 scraps <- readRDS(glue("./data/week{week}_scrap.rds"))
+
+# PROJECT FANTASY POINTS
+source("./R/import/ffa_player_projection.R")
 proj_table  <- calcPlayersProjections(scraps, read_yaml("./config/score_settings.yml"))
 
 # PLAYERS AND MATCHUPS ####
@@ -50,6 +46,47 @@ my_player_ids <- player_ids %>%
     id = as.integer(id), 
     nfl_id = as.integer(nfl_id))
 
+# TEST BRANCH: TEAM ROSTERS ####
+team_allocation <- teams_rosters %>% 
+  select(teamId, fantasy.team=name, rosters) %>% 
+  unnest(rosters) %>% 
+  select(teamId, fantasy.team, playerId)
+
+# tipos de status que zera a pontuacao
+injuryStatus <- c("Suspended","Injured Reserve","Not With Team")
+
+# pega as projecoes e cruza com player stats para ver status de injury
+players_projs <- proj_table %>% 
+  inner_join(my_player_ids, by="id") %>% # unifica os ids
+  inner_join(players_stats, by=c("nfl_id"="playerId")) %>% # adiciona info de status
+  # colina duplicada vinda do Join
+  select(-position.y) %>% 
+  rename(position=position.x) %>% 
+  # pega somente a semana de interesse
+  unnest(weekPts) %>% 
+  filter(week==week) %>% 
+  ## Zera Estatisticas de quem estah machucado
+  mutate(
+    points  = ifelse(injuryGameStatus %in% injuryStatus, 0, points),
+    floor   = ifelse(injuryGameStatus %in% injuryStatus, 0, floor),
+    ceiling = ifelse(injuryGameStatus %in% injuryStatus, 0, ceiling),
+    sd_pts  = ifelse(injuryGameStatus %in% injuryStatus, 0, sd_pts),
+  ) %>% 
+  # adiciona a informacao do time "owner"
+  left_join(team_allocation, by=c("nfl_id"="playerId")) %>% 
+  # quem nao tem time vira "Free Agent"
+  mutate(fantasy.team=if_else(is.na(fantasy.team),"*FreeAgent", fantasy.team))
+
+
+# salva estatisticas dos jogadores
+players_stats %>% 
+  unnest(weekPts) %>% 
+  inner_join(my_player_ids, by=c("playerId"="nfl_id")) %>% 
+  mutate(nfl_id=playerId) %>%
+  saveRDS("./data/players_points.rds")
+
+# salva projecoes  
+saveRDS(players_projs, glue("./data/week{week}_players_projections.rds"))
 
 # SIMULACAO ####
 
@@ -57,11 +94,33 @@ my_player_ids <- player_ids %>%
 source("./R/simulation/players_projections.R")
 site_ptsproj <- calcPointsProjection(season, read_yaml("./config/score_settings.yml"))
 pts_errors <- projectErrorPoints(players_stats, site_ptsproj, my_player_ids, week)
-# pts_flcl <- projectFloorCeiling(proj_table, week, season)
 
 # adiciona os erros de projeções passadas
 ptsproj <- site_ptsproj %>% # projecao dos sites
-  bind_rows(pts_errors) # erros das projecoes nas semanas passadas
+  bind_rows(pts_errors) 
+
+###### calcula 95% de intervado de confidencia em cima das projecoes e dos erros
+
+# aplica o teste sobre os pontos calculados e coloca em formato tidy
+tidy.ttest <- function(x) broom::tidy(t.test(x))
+# versao segura que nao falha se nao der para colocar NULL
+sttest <- safely(tidy.ttest)
+
+# pega os pontos projetados (com erros) da semana em questão
+.week<-week
+ptsproj %>% 
+  filter(week==.week) %>% 
+  select(id, data_src, pts.proj) %>%
+  # para cada jogador aninha a projecao de pontos e aplica ttest
+  group_by(id) %>% 
+  nest() %>% 
+  ungroup() %>% 
+  mutate(stats = map(data, function(.x){
+    sttest(.x$pts.proj)$result
+  })) %>% 
+  unnest(stats) %>% 
+  select(id, estimate, conf.low, conf.high, data) %>% 
+  saveRDS(glue("./data/dudesffa_projpoints_week{week}.rds"))
 
 # simulação das partidas
 source(glue("./R/simulation/points_simulation_v{sim.version}.R"))
@@ -70,13 +129,23 @@ sim <- simulateGames(week, season, ptsproj, matchups_games, teams_rosters, playe
 # salva resultado
 saveRDS(sim, glue("./data/simulation_v{sim.version}_week{week}_{prefix}.rds"))
 
-# # constroi o relatório
+###### render reports
+# PROJECTION REPORT ####
+rmarkdown::render(
+  input = "./R/reports/ffa_players_projection.Rmd",
+  output_file = glue("../../{destPath}/ffa_players_projection_week{week}.html"),
+  output_format = "flex_dashboard",
+  params = list(week=week)
+)
+
+
 rmarkdown::render(
   input = glue("./R/reports/dudes_simulation_v{sim.version}.Rmd"),
   output_file = glue("../../{destPath}/dudes_simulation_v{sim.version}_week{week}_{prefix}.html"),
   output_format = "flex_dashboard",
   params = list(week=week, prefix=prefix)
 )
+
 
 ## Extrai o ranking
 
