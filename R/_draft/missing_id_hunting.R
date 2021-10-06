23834a2f264446dbab4c14b9a64baa1f23974ff0
Giuliano Sposito <gsposito@ciandt.com>
2021-10-06 14:58:47 -0300

    Atualizando simulacoes com o ID do MacLoaughlin

--- R/_draft/missing_id_hunting.R
+++ R/_draft/missing_id_hunting.R
@@ -1,4 +1,5 @@
 evan <- 15368
+chase <- 14717
 
 yahooScrap %>% 
   filter(position=="K") %>%
@@ -8,13 +9,18 @@ webScraps[["K"]] %>%
   filter(id==evan)
 
 scraps[["K"]] %>% 
-  filter(id==evan)
+  filter(str_detect(player, "Evan") | str_detect(player, "McLaughlin")) %>%
+  projections_table()
+
+player_ids %>% 
+  filter(id %in% c(evan, chase))
+
 
 proj_table %>% 
   filter(id==evan)
 
 players_stats %>% 
-  filter(firstName=="Evan")
+  filter(firstName=="Chase")
 
 evanId <- 2565953
 
@@ -41,3 +47,17 @@ players_stats %>%
 players_projs %>% 
   filter(id==evan) %>% 
   glimpse()
+
+allscraps 
+
+scraps <- list("K"=allscraps$K)
+attr(scraps, "season") <- 2021
+attr(scraps, "week") <- 5
+
+allscraps$K %>% class()
+
+proj_table %>% 
+  filter(first_name %in% c("Chase","Evan"))
+
+ptsproj %>% 
+  filter(pos=="K", id==chase)
