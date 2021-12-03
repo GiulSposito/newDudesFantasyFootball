2587d0e5118664dda18afc574e27999017945688
Giuliano Sposito <gsposito@MMB026334CPS.local>
2021-12-03 07:43:28 -0300

    update pos TNF

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -12,7 +12,7 @@ options(dplyr.summarise.inform = FALSE)
 week <- 13
 season <- 2021
 config <- read_yaml("./config/config.yml")
-prefix <- "posWaivers"
+prefix <- "posTNF"
 destPath <- "static/reports/2021"
 sim.version <- 5
 
