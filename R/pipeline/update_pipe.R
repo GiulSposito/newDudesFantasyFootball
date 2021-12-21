8e8966cfaf3b8d380468fdccf5d799bedfe8c253
Giuliano Sposito <gsposito@MMB026334CPS.local>
2021-12-21 08:24:30 -0300

    Update pre Tusday

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -12,7 +12,7 @@ options(dplyr.summarise.inform = FALSE)
 week <- 15
 season <- 2021
 config <- read_yaml("./config/config.yml")
-prefix <- "preMondayGames"
+prefix <- "preTuesdayGames"
 destPath <- "static/reports/2021"
 sim.version <- 5
 
