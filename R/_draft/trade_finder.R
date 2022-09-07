8aafeeb65bbec157fca227a5999cf9ed847e52fe
Giuliano Sposito <gsposito@MMB026334CPS.local>
2022-09-07 13:02:42 -0300

    Esboço para o código de análise de traders

---
+++ R/_draft/trade_finder.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+
+season <- readRDS("./data/season_proj_stats.rds")
+week <- readRDS("./data/week1_players_projections.rds")
+
+season %>% 
+  filter( avg_type=="average") %>% 
+  select( id, pos, first_name, last_name, team, everything()) %>% 
+  filter( id %in% c(12650,12652)) %>% 
+  mutate( name = glue::glue("{first_name} {last_name} {team} ({pos})") ) %>% 
+  mutate( name = fct_reorder(name, points)) %>% 
+  ggplot(aes(y=points, x=name)) +
+  geom_pointrange(aes(ymin=floor, ymax=ceiling)) +
+  geom_point() +
+  coord_flip() +
+  labs(x="player", y="season points") +
+  theme_light()
+
+sim <- readRDS("./data/simulation_v5_week1_posWaivers.rds")
+
+sim$teams %>% 
+  select(teamId, name)
+
+tibble(
+  tradeId = 1,
+  propTeamId = 1,
+  toTeamId = 2,
+  sendPlayer = 12650,
+  recvPlayer = 12652
+)
+
+week %>% 
+  filter( avg_type=="average") %>% 
+  select( id, pos, first_name, last_name, team, injuryGameStatus, rankAgainstPosition, everything()) %>% 
+  filter( id %in% c(12650,12652)) %>% 
+  inner_join(readRDS("./data/injuryGameStatusAbbr.rds"), by = "injuryGameStatus") %>% 
+  mutate( injuryAbbr = if_else(is.na(injuryAbbr), "", paste0("(", injuryAbbr, ")"))) %>% 
+  mutate( name = glue::glue("{first_name} {last_name} {team} {injuryAbbr}") ) %>% 
+  mutate( name = fct_reorder(name, points)) %>% 
+  ggplot(aes(y=points, x=name)) +
+  geom_pointrange(aes(ymin=floor, ymax=ceiling)) +
+  geom_point() +
+  coord_flip() +
+  labs(x="player", y="week 1 points") +
+  theme_light()
+
+readRDS("./data/injuryGameStatusAbbr.rds")
