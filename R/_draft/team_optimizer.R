734621779da879d99957eef3ae8491cbd137a084
Giuliano Sposito <gsposito@MMB026334CPS.local>
2022-10-05 23:01:19 -0300

    draft de simulação baseada no fit da distribuição

--- R/_draft/team_optimizer.R
+++ R/_draft/team_optimizer.R
@@ -3,10 +3,11 @@ library(glue)
 
 .team <- "Amparo Bikers"
 .week <- 5
-.prefix <- "preWaivers"
 
+# dados dos jogadores
 players <- readRDS(glue("./data/week{.week}_players_projections.rds")) %>% 
   filter(
+    avg_type=="average",
     fantasy.team %in% c(.team,"*FreeAgent"),
     team!="FA",
     week==.week
@@ -16,12 +17,63 @@ players <- readRDS(glue("./data/week{.week}_players_projections.rds")) %>%
     floor   = if_else(injuryGameStatus=="Out", 0, floor), 
     points  = if_else(injuryGameStatus=="Out", 0, points), 
     ceiling = if_else(injuryGameStatus=="Out", 0, ceiling)
-  )
+  ) %>% 
+  select(id, playerId=nfl_id, name, position, nflTeam = nflTeamAbbr, week, weekPts, weekSeasonPts, teamId, fantasy.team, injuryGameStatus)
+
+# projeção de dados "Dudes"
+dudes_proj <- readRDS("./data/points_projection_and_errors.rds") %>% 
+  filter(week==.week)
+
+player_proj_points %>% 
+  filter(id==14783, week==.week) %>% 
+  select(pts.proj) %>% 
+  unnest(pts.proj) %>% 
+  ggplot(aes(x=pts.proj)) +
+  geom_histogram(alpha=.5) +
+  theme_light()
+
+ecdfun <- dudes_proj %>% 
+  filter(id==14783, week==.week) %>% 
+  pull(pts.proj) %>% 
+  ecdf()
+
+pdfden <- ecdfun %>% 
+  knots() %>% 
+  density(from=0, to=100)
+
+pdfden %>% 
+  approxfun()
+
+random.points <- approx(
+  cumsum(pdfden$y)/sum(pdfden$y),
+  pdfden$x,
+  runif(10000)
+)$y
+
+hist(random.points,100)
+
+random.points %>% 
+  quantile(probs=c(.1,.5,.9))
+
+dudes_proj %>% 
+  filter(id==14783, week==.week) %>% 
+  pull(pts.proj) %>% 
+  quantile(probs=c(.1,.5,.9))
+
+   t.test() %>% 
+  broom::tidy()
+
+
+
+seq(10:40) %>% 
+  map_dbl(pdfun)
 
 
 pstats <- readRDS("./data/players_points.rds") %>% 
   filter(week==.week) %>% 
   select(playerId, weekSeasonPts )
+
+
   
 # 
 # players <- players %>% 
