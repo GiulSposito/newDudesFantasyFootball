e6d7d1aa3fec79abee92a807c9be8cb46b7ad7bd
Giuliano Sposito <gsposito@MMB026334CPS.local>
2022-10-19 08:07:10 -0300

    pos Waivers

--- R/_draft/team_optimizer.R
+++ R/_draft/team_optimizer.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(glue)
 
 .team <- "Amparo Bikers"
-.week <- 6
+.week <- 7
 
 # readRDS(glue("./data/week{.week}_players_projections.rds")) %>% 
 #   select(teamId, fantasy.team) %>% 
