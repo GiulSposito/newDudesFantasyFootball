fd36e25aeadf99d517fc90c907a5144a3c6a1506
Giuliano Sposito <gsposito@MMB026334CPS.local>
2022-10-31 09:18:34 -0300

    pos SNF

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -9,10 +9,10 @@ library(yaml)
 options(dplyr.summarise.inform = FALSE)
 
 # EXECUTION PARAMETERS ####
-week <- 3
+week <- 8
 season <- 2022
 config <- yaml::read_yaml("./config/config.yml")
-prefix <- "final"
+prefix <- "posSNF"
 destPath <- "static/reports/2022"
 sim.version <- 5
 
