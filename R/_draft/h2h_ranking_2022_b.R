45d2e556682b0834884330fd4efad4275958557e
Giuliano Sposito <gsposito@MMB026334CPS.local>
2022-11-14 08:44:02 -0300

    update posSNF

--- R/_draft/h2h_ranking_2022_b.R
+++ R/_draft/h2h_ranking_2022_b.R
@@ -1,35 +1,36 @@
-# install.packages("nflseedR")
-library(nflseedR)
-
-?compute_division_ranks
-
-games <- tibble(
-  sim = 1,
-  game_type = "REG",
-  week = c(1,1,2,2,3,3),
-  away_team = c("A","C","A","B","A","B"),
-  home_team = c("B","D","C","D","D","C"),
-  result = c(1,-1000,-1,1,1,-1)
-)
-
-teams <- tibble(
-  sim = 1,
-  team = c("A","B","C","D"),
-  conf = "dudes", 
-  division = "dudes"
-)
-
-res <- compute_division_ranks(games, teams, 2, T)
-
-res
-
-res$h2h %>% 
-  pivot_wider(id_cols=c(sim, team), names_from = opp, values_from = h2h_wins)
+# # install.packages("nflseedR")
+# library(nflseedR)
+# 
+# ?compute_division_ranks
+# 
+# games <- tibble(
+#   sim = 1,
+#   game_type = "REG",
+#   week = c(1,1,2,2,3,3),
+#   away_team = c("A","C","A","B","A","B"),
+#   home_team = c("B","D","C","D","D","C"),
+#   result = c(1,-1000,-1,1,1,-1)
+# )
+# 
+# teams <- tibble(
+#   sim = 1,
+#   team = c("A","B","C","D"),
+#   conf = "dudes", 
+#   division = "dudes"
+# )
+# 
+# res <- compute_division_ranks(games, teams, 2, T)
+# 
+# res
+# 
+# res$h2h %>% 
+#   pivot_wider(id_cols=c(sim, team), names_from = opp, values_from = h2h_wins)
 
 
 library(tidyverse)
 library(glue)
 source("./R/api/ffa_league.R")
+library(nflseedR)
 config <- yaml::read_yaml("./config/config.yml")
 
 ### DATA SET PREPARATION
@@ -43,7 +44,7 @@ teams <- sim$teams %>%
   select(id=teamId, name=nickname)
 
 
-games <- 1:8 %>% 
+games <- 1:10 %>% 
   map_df(function(.w){
     
     sim <- glue("./data/simulation_v5_week{.w}_final.rds") %T>% 
