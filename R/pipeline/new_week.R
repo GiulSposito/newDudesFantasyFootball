53910e28170a46abd01b2bdf0a8fbb2701c7f4dd
Giuliano Sposito <gsposito@MMB026334CPS.local>
2023-10-05 11:29:22 -0300

    pos waivers update

--- R/pipeline/new_week.R
+++ R/pipeline/new_week.R
@@ -12,7 +12,7 @@ options(dplyr.summarise.inform = FALSE)
 week <- 5
 season <- 2023
 config <- read_yaml("./config/config.yml")
-prefix <- "preWaivers"
+prefix <- "posWaivers"
 destPath <- "static/reports/2023"
 rep.version <- 5
 sim.version <- 6
