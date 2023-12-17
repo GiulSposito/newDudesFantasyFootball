0fba90fd3e2973a64af4a28a789af8fd980ec526
Giuliano Sposito <gsposito@MMB026334CPS.local>
2023-12-17 13:35:34 -0300

    w15 pre sunday games

--- R/pipeline/update_pipe.R
+++ R/pipeline/update_pipe.R
@@ -12,7 +12,7 @@ options(dplyr.summarise.inform = FALSE)
 week <- 15
 season <- 2023
 config <- read_yaml("./config/config.yml")
-prefix <- "posTNF"
+prefix <- "preSunday"
 destPath <- "static/reports/2023"
 rep.version <- 5
 sim.version <- 6
