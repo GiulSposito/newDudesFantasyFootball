373a72e3aa73e513d98961628b127d3dd1643089
Giuliano Sposito <gsposito@gmail.com>
2024-10-03 11:55:17 -0300

    preTNF w5

--- R/pipeline/new_week.R
+++ R/pipeline/new_week.R
@@ -13,7 +13,7 @@ week <- 5
 .week<-week
 season <- 2024
 config <- read_yaml("./config/config.yml")
-prefix <- "preWaivers"
+prefix <- "preTNF"
 destPath <- "static/reports/2024"
 rep.version <- 5
 sim.version <- 6
