c2cfbb99953cbada5791642c4c977eebc0125419
Giuliano Sposito <gsposito@gmail.com>
2024-10-27 13:20:03 -0300

    w8 pre sunday games

--- R/pipeline/new_week.R
+++ R/pipeline/new_week.R
@@ -13,7 +13,7 @@ week <- 8
 .week<-week
 season <- 2024
 config <- read_yaml("./config/config.yml")
-prefix <- "posWaivers"
+prefix <- "preSundayGames"
 destPath <- "static/reports/2024"
 rep.version <- 5
 sim.version <- 6
