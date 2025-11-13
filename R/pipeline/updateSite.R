65689f432d0ac1390531882bafc5af312bf648d5
Giuliano Sposito <gsposito@gmail.com>
2025-11-13 19:23:11 -0300

    w11 preTNF

--- R/pipeline/updateSite.R
+++ R/pipeline/updateSite.R
@@ -11,7 +11,7 @@ options(dplyr.summarise.inform = FALSE, blogdown.server.timeout = 600)
 # EXECUTION PARAMETERS ####
 week <- 11
 updateScrapProj <- T
-prefix <- "posWaivers"
+prefix <- "preTNF"
 .week<-week
 season <- 2025
 config <- read_yaml("./config/config.yml")
