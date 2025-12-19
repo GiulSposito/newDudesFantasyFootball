f826ce6a02863d09cf8c5ad56fd606feb6aeadf0
Giuliano Sposito <gsposito@gmail.com>
2025-12-19 09:22:31 -0300

    w16 posTNF

--- R/pipeline/updateSite.R
+++ R/pipeline/updateSite.R
@@ -10,8 +10,8 @@ options(dplyr.summarise.inform = FALSE, blogdown.server.timeout = 600)
 
 # EXECUTION PARAMETERS ####
 week <- 16
-updateScrapProj <- T
-prefix <- "preTNF"
+updateScrapProj <- F
+prefix <- "posTNF"
 .week<-week
 season <- 2025
 config <- read_yaml("./config/config.yml")
